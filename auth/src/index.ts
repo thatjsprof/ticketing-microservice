fc2215cce47199477393e3d3939e29918da35818
davetech <davidrums65@gmail.com>
2023-11-12 22:53:07 +0100

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+import { json } from "body-parser";
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(json());
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
